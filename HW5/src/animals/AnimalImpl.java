2e4de4438b49eb1815a315f3322365d0f7304bed
dmitrybezro <d.bezrodnow@gmail.com>
2021-08-03 17:35:00 +0300

    Fix builder. Builder in the parent class is replaced by specific implementations in the child ones.

--- HW5/src/animals/AnimalImpl.java
+++ HW5/src/animals/AnimalImpl.java
@@ -1,39 +1,14 @@
 package animals;
 
 import model.Animal;
-import model.Nutrition;
 import model.Species;
 
 public class  AnimalImpl implements Animal {
-    private final String name;
-    private double height;
-    private double weight;
-    private int age;
-    private int numberCage;
-    //private final Nutrition NUTRITION;
-
-    protected AnimalImpl(AnimalBuilder animalBuilder){
-        //  Исключения для отрицательных значений
-
-            if (animalBuilder.name == null || animalBuilder.name.isEmpty()) {
-                throw new IllegalArgumentException("Enter the name of the animal");
-            }
-            if (animalBuilder.height < 0) {
-                throw new IllegalArgumentException("The animal's height must be greater than zero");
-            }
-            if (animalBuilder.weight < 0) {
-                throw new IllegalArgumentException("The animal's weight must be greater than zero");
-            }
-            if (animalBuilder.age < 0) {
-                throw new IllegalArgumentException("The animal's age must be greater than zero");
-            }
-
-
-        this.name = animalBuilder.name;
-        this.height = animalBuilder.height;
-        this.weight = animalBuilder.weight;
-        this.age = animalBuilder.age;
-    }
+    protected String name;
+    protected double height;
+    protected double weight;
+    protected int age;
+    protected int numberCage;
 
     public double getHeight() {
         return this.height;
@@ -50,10 +25,6 @@ public class  AnimalImpl implements Animal {
     public void setWeight(double weight) {
         this.weight = weight;
     }
-
-/*    public Nutrition getNutrition() {
-        return this.NUTRITION;
-    }*/
 
     public int getAge() {
         return this.age;
@@ -77,43 +48,5 @@ public class  AnimalImpl implements Animal {
     @Override
     public Species getSpecies() {
         return null;
-    }
-
-    //  Статистический вложенный класс строитель
-    public static class AnimalBuilder {
-        private String name;
-        private double height;
-        private double weight;
-        private int age;
-        private int numberCage;
-
-
-        public AnimalBuilder(){
-            super();
-        }
-
-        public AnimalBuilder name(String name){
-            this.name = name;
-            return this;
-        }
-
-        public AnimalBuilder height(double height){
-            this.height = height;
-            return this;
-        }
-
-        public AnimalBuilder weight(double weight){
-            this.weight = weight;
-            return this;
-        }
-
-        public AnimalBuilder age(int age){
-            this.age = age;
-            return this;
-        }
-
-        public AnimalImpl build(){
-            return new AnimalImpl(this);
-        }
     }
 }
--- HW5/src/animals/Squirrel.java
+++ HW5/src/animals/Squirrel.java
@@ -1,6 +1,5 @@
 package animals;
 
-import model.Animal;
 import model.Nutrition;
 import model.Species;
 
@@ -8,8 +7,25 @@ public class Squirrel extends AnimalImpl {
     private int numberCage;
     private final Nutrition NUTRITION = Nutrition.OMNIVOROUS;
 
-    public Squirrel(AnimalImpl.AnimalBuilder animalBuilder){
-        super(animalBuilder);
+    protected Squirrel(SquirrelBuilder animalBuilder){
+
+        if (animalBuilder.name == null || animalBuilder.name.isEmpty()) {
+            throw new IllegalArgumentException("Enter the name of the animal");
+        }
+        if (animalBuilder.height < 0) {
+            throw new IllegalArgumentException("The animal's height must be greater than zero");
+        }
+        if (animalBuilder.weight < 0) {
+            throw new IllegalArgumentException("The animal's weight must be greater than zero");
+        }
+        if (animalBuilder.age < 0) {
+            throw new IllegalArgumentException("The animal's age must be greater than zero");
+        }
+
+        this.name = animalBuilder.name;
+        this.height = animalBuilder.height;
+        this.weight = animalBuilder.weight;
+        this.age = animalBuilder.age;
     }
 
     public Nutrition getNutrition() {
@@ -25,5 +41,41 @@ public class Squirrel extends AnimalImpl {
     @Override
     public Species getSpecies() {
         return Species.SQUIRREL;
+    }
+
+    public static class SquirrelBuilder {
+        private String name;
+        private double height;
+        private double weight;
+        private int age;
+        private int numberCage;
+
+        public SquirrelBuilder (){
+            super();
+        }
+
+        public SquirrelBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public SquirrelBuilder height(double height){
+            this.height = height;
+            return this;
+        }
+
+        public SquirrelBuilder weight(double weight){
+            this.weight = weight;
+            return this;
+        }
+
+        public SquirrelBuilder age(int age){
+            this.age = age;
+            return this;
+        }
+
+        public Squirrel build(){
+            return new Squirrel(this);
+        }
     }
 }
